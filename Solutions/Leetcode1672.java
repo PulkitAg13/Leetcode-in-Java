689baa8dd592f6cbb8db4760660fb8afb3dc8321
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-18 01:30:08 +0530

    Update Leetcode1672.java

--- Solutions/Leetcode1672.java
+++ Solutions/Leetcode1672.java
@@ -1,5 +1,17 @@
 package Solutions;
 
 public class Leetcode1672 {
-    
+    class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for(int[] row : accounts){
+            int count = 0;
+            for(int col : row){
+                count = count + col;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
 }
