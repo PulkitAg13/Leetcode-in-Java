255fcc7c86acf165b62277885ffad285af009630
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-18 01:21:54 +0530

    Create Leetcode1295.java

---
+++ Solutions/Leetcode1295.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+public class Leetcode1295 {
+    class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums){
+            int digits = (int)(Math.log10(num)) + 1;
+            if(digits % 2 == 0){
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
+}
