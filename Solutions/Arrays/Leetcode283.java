50461f0fedb1fd4724dc256495321f5b1ff0d722
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-20 15:24:17 +0530

    Create Leetcode283.java

---
+++ Solutions/Arrays/Leetcode283.java
@@ -0,0 +1,21 @@
+package Solutions.Arrays;
+
+public class Leetcode283 {
+    class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index = index + 1;
+            }
+        }
+
+        while (index < nums.length) {
+            nums[index] = 0;
+            index = index + 1;
+        }
+    }
+}
+}
