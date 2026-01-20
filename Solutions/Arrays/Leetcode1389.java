6245a51aee9e21bd4dcf51fad5844a105883bc14
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-20 15:17:49 +0530

    Create Leetcode1389.java

---
+++ Solutions/Arrays/Leetcode1389.java
@@ -0,0 +1,24 @@
+package Solutions.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode1389 {
+    class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> targetList = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            targetList.add(index[i], nums[i]);
+        }
+        
+        int[] target = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            target[i] = targetList.get(i);
+        }
+        
+        return target;
+    }
+}
+
+}
