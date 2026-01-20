6cb83268f7ce2f9252b531b023ca0a37e5df6242
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-20 15:24:12 +0530

    Create Leetcode217.java

---
+++ Solutions/Arrays/Leetcode217.java
@@ -0,0 +1,17 @@
+package Solutions.Arrays;
+
+import java.util.Arrays;
+
+public class Leetcode217 {
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+}
