41e17e9b85cbd47f385bb53064f512f70a45fd06
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-21 01:15:45 +0530

    Create Leetcode1773.java

---
+++ Solutions/Arrays/Leetcode1773.java
@@ -0,0 +1,29 @@
+package Solutions.Arrays;
+
+import java.util.List;
+
+public class Leetcode1773 {
+    class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int col = 0;
+
+        if (ruleKey.equals("type")) {
+            col = 0;
+        } else if (ruleKey.equals("color")) {
+            col = 1;
+        } else {
+            col = 2;
+        }
+
+        int count = 0;
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).get(col).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+}
