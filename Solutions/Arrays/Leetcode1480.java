ed51363fd7cc0890c32c51791a4c74f8460c657c
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-30 16:48:11 +0530

    Update Leetcode1480.java

--- Solutions/Arrays/Leetcode1480.java
+++ Solutions/Arrays/Leetcode1480.java
@@ -3,14 +3,10 @@ package Solutions.Arrays;
 public class Leetcode1480 {
     class Solution {
     public int[] runningSum(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n];
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            sum = sum + nums[i];
-            arr[i] = sum;
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i - 1] + nums[i];
         }
-        return arr;
+        return nums;
     }
 }
 }
