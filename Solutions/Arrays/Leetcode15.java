1e704569c7cf6b2f3bad6cc7a26c3c52f0b3e5f4
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-01 21:23:03 +0530

    Create Leetcode15.java

---
+++ Solutions/Arrays/Leetcode15.java
@@ -0,0 +1,41 @@
+package Solutions.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode15 {
+    class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int z = nums[i];
+            int l = i + 1;
+            int r = nums.length-1;
+            while(l < r){
+                int x = nums[l];
+                int y = nums[r];
+                int sum = x + y +z;
+                if(sum == 0){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(z);
+                    temp.add(x);
+                    temp.add(y);
+                    ans.add(temp);
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
+                }else if(sum > 0){
+                    r--;
+                }else{
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+}
+}
