40e73c4fd7dd6a9ef1cdfca0257b07c9b2859ac4
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-02 17:14:00 +0530

    Create Leetcode27.java

---
+++ Solutions/Arrays/Leetcode27.java
@@ -0,0 +1,19 @@
+package Solutions.Arrays;
+
+public class Leetcode27 {
+    class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int k = nums.length;
+        while (i < k) {
+            if (nums[i] == val) {
+                nums[i] = nums[k-1];
+                k--;
+            } else {
+                i++;
+            }
+        }
+        return k;
+    }
+}
+}
