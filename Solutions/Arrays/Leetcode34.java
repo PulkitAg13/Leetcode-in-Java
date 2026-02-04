40c85ac86a4fc7de83f0cfb532cb0fa1aabe9132
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-04 15:45:04 +0530

    Create Leetcode34.java

---
+++ Solutions/Arrays/Leetcode34.java
@@ -0,0 +1,22 @@
+package Solutions.Arrays;
+
+public class Leetcode34 {
+    class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                result[0] = i;
+                while (i < nums.length && nums[i] == target) {
+                    i++;
+                }
+                result[1] = i - 1;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
+}
