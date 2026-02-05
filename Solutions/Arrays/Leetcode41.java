068f2c54a90714190c3f7816ceb08860e0de9913
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-05 18:53:23 +0530

    Create Leetcode41.java

---
+++ Solutions/Arrays/Leetcode41.java
@@ -0,0 +1,29 @@
+package Solutions.Arrays;
+
+public class Leetcode41 {
+    class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+            if (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
+
+}
