3f5c2ef3ab7c26cd5050d060e41aa51ec9ba8d38
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-05 20:25:40 +0530

    Create Leetcode1456.java

---
+++ Solutions/Strings/Leetcode1456.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class Leetcode1456 {
+    class Solution {
+    public int maxVowels(String s, int k) {
+        int n = s.length();
+        int count = 0;
+        int vowel = 0;
+        for(int i = 0; i < k; i++){
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        vowel = count;
+        for(int i = 0; i < n-k; i++){
+            char ch = s.charAt(i);
+            char ck = s.charAt(k+i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count--;
+            }
+            if (ck == 'a' || ck == 'e' || ck == 'i' || ck == 'o' || ck == 'u') {
+                count++;
+            }
+            if(count > vowel){
+                vowel = count;
+            }
+        }
+        return vowel;
+    }
+}
+}
