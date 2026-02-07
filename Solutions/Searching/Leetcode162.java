173e03da6495fd695157fcd1233aa2174403f51e
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-08 02:07:10 +0530

    Create Leetcode162.java

---
+++ Solutions/Searching/Leetcode162.java
@@ -0,0 +1,19 @@
+package Solutions.Searching;
+
+public class Leetcode162 {
+    class Solution {
+    public int findPeakElement(int[] nums) {
+        int s = 0;
+        int e = nums.length-1;
+        while(s < e){
+            int m = s + (e - s)/2;
+            if(nums[m] > nums[m + 1]){
+                e = m;
+            }else{
+                s = m + 1;
+            }
+        }
+        return s;
+    }
+}
+}
