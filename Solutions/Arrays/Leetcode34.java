f3c45598139d8c42636f009b7adfd502e3d49447
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-07 23:38:34 +0530

    Update Leetcode34.java

--- Solutions/Arrays/Leetcode34.java
+++ Solutions/Arrays/Leetcode34.java
@@ -4,18 +4,33 @@ public class Leetcode34 {
     class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = {-1, -1};
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                result[0] = i;
-                while (i < nums.length && nums[i] == target) {
-                    i++;
-                }
-                result[1] = i - 1;
-                break;
+        int s = 0;
+        int e = nums.length - 1;
+        while(s <= e){
+            int m = s + (e - s)/2;
+            if(target > nums[m]){
+                s = m + 1;
+            }else if(target < nums[m]){
+                e = m - 1;
+            }else{
+                result[0] = m;
+                e = m - 1;
             }
         }
 
+        s = 0;
+        e = nums.length - 1;
+        while(s <= e){
+            int m = s + (e - s)/2;
+            if(target > nums[m]){
+                s = m + 1;
+            }else if(target < nums[m]){
+                e = m - 1;
+            }else{
+                result[1] = m;
+                s = m + 1;
+            }
+        }
         return result;
     }
 }
