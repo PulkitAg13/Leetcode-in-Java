0e3c7e611a8342d86375da5892c3f01ef25555a5
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-09 00:50:02 +0530

    Create Leetcode375.java

---
+++ Solutions/Searching/Leetcode375.java
@@ -0,0 +1,35 @@
+package Solutions.Searching;
+
+public class Leetcode375 {
+    /** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+public class GuessGame {
+    public int guess(int num) {
+        return 0;
+    }
+}
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int s = 1;
+        int e = n;
+        while(s <= e){
+            int m = s + (e - s) / 2;
+            int ans = guess(m);
+            if(ans == 0){
+                return m;
+            }else if(ans == -1){
+                e = m - 1;
+            }else{
+                s = m + 1;
+            }
+        }
+        return -1;
+    }
+}
+}
