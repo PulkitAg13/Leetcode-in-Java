6b7cd87d4687b40cde6dd1d7b420724c76813ed0
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-15 17:05:34 +0530

    Update Leetcode33.java

--- Solutions/Searching/Leetcode33.java
+++ Solutions/Searching/Leetcode33.java
@@ -3,62 +3,31 @@ package Solutions.Searching;
 public class Leetcode33 {
     class Solution {
     public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
+        int start = 0, end = nums.length - 1;
 
-        // if you did not find a pivot, it means the array is not rotated
-        if (pivot == -1) {
-            // just do normal binary search
-            return binarySearch(nums, target, 0 , nums.length - 1);
-        }
-
-        // if pivot is found, you have found 2 asc sorted arrays
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-        
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    // this will not work in duplicate values
-    int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid-1;
-            }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+
+            if (nums[mid] == target) return mid;
+
+            // Check if left part is sorted
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1; // move left
+                } else {
+                    start = mid + 1; // move right
+                }
+            } 
+            // Right part is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1; // move right
+                } else {
+                    end = mid - 1; // move left
+                }
             }
         }
+
         return -1;
     }
 }
