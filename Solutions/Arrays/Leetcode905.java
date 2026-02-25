acb6639dd6aa6e5c0add79d8464384b4bf9573df
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-25 18:14:03 +0530

    Update Leetcode905.java

--- Solutions/Arrays/Leetcode905.java
+++ Solutions/Arrays/Leetcode905.java
@@ -3,19 +3,17 @@ package Solutions.Arrays;
 public class Leetcode905 {
     class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int[] ans = new int[nums.length];
         int l = 0;
         int r = nums.length - 1;
-        for(int k = 0; k < nums.length; k++){
-            if(nums[k] % 2 == 0){
-                ans[l] = nums[k];
-                l++;
-            }else{
-                ans[r] = nums[k];
-                r--;
-            }
+        while (l < r) {
+            if (nums[l] % 2 > nums[r] % 2) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+            }else if (nums[l] % 2 == 0) l++;
+            else r--;
         }
-        return ans;
+        return nums;
     }
 }
 }
