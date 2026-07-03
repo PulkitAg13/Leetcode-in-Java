e6ccc94563e4bde9a3eececffdf57e7551a5e98c
PulkitAg13 <pulkit.ag13@gmail.com>
2026-07-04 02:46:20 +0530

    Create Leetcode125.java

---
+++ Solutions/Strings/Leetcode125.java
@@ -0,0 +1,19 @@
+package Solutions.Strings;
+
+public class Leetcode125 {
+    class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        String str = sb.toString();
+        String rev = sb.reverse().toString();
+
+        return str.equals(rev);
+    }
+}
+}
