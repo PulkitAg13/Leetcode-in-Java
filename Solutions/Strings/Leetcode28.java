e0374401d9f18bbcde11d20910ef23c90ea30c1d
PulkitAg13 <pulkit.ag13@gmail.com>
2026-07-08 16:02:35 +0530

    Update Leetcode28.java

--- Solutions/Strings/Leetcode28.java
+++ Solutions/Strings/Leetcode28.java
@@ -3,17 +3,7 @@ package Solutions.Strings;
 public class Leetcode28 {
     class Solution {
     public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        
-        if (m > n) return -1;
-        
-        for (int i = 0; i <= n - m; i++) {
-            if (haystack.substring(i, i + m).equals(needle)) {
-                return i;
-            }
-        }
-        return -1;
+        return haystack.indexOf(needle);
     }
 }
 }
