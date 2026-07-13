9a7bd4ef0a9b0f5a0b60fb240ab4eae7b08aaeed
PulkitAg13 <pulkit.ag13@gmail.com>
2026-07-13 13:25:15 +0530

    Create Leetcode1513.java

---
+++ Solutions/Strings/Leetcode1513.java
@@ -0,0 +1,18 @@
+package Solutions.Strings;
+
+public class Leetcode1513 {
+    class Solution {
+    public int numSub(String s) {
+        long cnt = 0, total = 0, mod = 1000000007;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            total = (total + cnt) % mod;
+        }
+        return (int) total;
+    }
+}
+}
