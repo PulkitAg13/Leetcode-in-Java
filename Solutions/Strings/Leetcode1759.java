93eb50aaf36972483ed2b802b703f094aefe58a3
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-07-13 23:35:01 +0530

    Implement countHomogenous method in Leetcode1759

--- Solutions/Strings/Leetcode1759.java
+++ Solutions/Strings/Leetcode1759.java
@@ -1,1 +1,20 @@
+package Solutions.Strings;
 
+public class Leetcode1759 {
+  class Solution {
+    public int countHomogenous(String s) {
+        long cnt = 0, total = 0, mod = 1000000007;
+        char ch = s.charAt(0);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ch) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            ch = s.charAt(i);
+            total = (total + cnt) % mod;
+        }
+        return (int) total;
+    }
+  }
+}
