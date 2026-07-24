55d8702368fe66a56b6642e378277829615f86a2
PulkitAg13 <pulkit.ag13@gmail.com>
2026-07-24 18:42:16 +0530

    Update Leetcode832.java

--- Solutions/Bitwise/Leetcode832.java
+++ Solutions/Bitwise/Leetcode832.java
@@ -1,5 +1,19 @@
 package Solutions.Bitwise;
 
 public class Leetcode832 {
-    
+    class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] arr : image){
+            int i = 0, j = arr.length - 1;
+            while (i <= j) {
+                int t = arr[i];
+                arr[i] = arr[j] ^ 1;
+                arr[j] = t ^ 1;
+                i++;
+                j--;
+            }
+        }
+        return image;
+    }
+}
 }
