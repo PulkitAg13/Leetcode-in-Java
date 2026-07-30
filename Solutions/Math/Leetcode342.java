b706545591355316f038a94dc4e73ca561438adc
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-07-30 22:46:36 +0530

    Implement isPowerOfFour method in Leetcode342

---
+++ Solutions/Math/Leetcode342.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n == 1){
+            return true;
+        }
+        while(n > 4){
+            if(n % 4 == 0){
+                n = n / 4;
+            }else{
+                return false;
+            }
+        }
+        if(n == 4) return true;
+        return false;
+    }
+}
