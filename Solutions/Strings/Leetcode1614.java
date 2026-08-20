ed79cc6e4471cead9674faf10d1631a4a7fa4f98
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-08-21 00:20:36 +0530

    Implement maxDepth method for parentheses string

---
+++ Solutions/Strings/Leetcode1614.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int ans = 0;
+        for(int  i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                count++;
+                ans = Math.max(count, ans);
+            }else if(s.charAt(i) == ')'){
+                count--;
+            }else{
+                continue;
+            }
+        }
+        return ans;
+    }
+}
