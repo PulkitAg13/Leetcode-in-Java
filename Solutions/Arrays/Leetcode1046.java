8308e55702a5efba71a7537cee7c12ba102c89be
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-08-28 23:11:20 +0530

    Implement lastStoneWeight method in Leetcode1046

---
+++ Solutions/Arrays/Leetcode1046.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+        for(int i = 0; i < n - 1; i++){
+            Arrays.sort(stones);
+            stones[n-1] = stones[n-1] - stones[n-2];
+            stones[n-2] = 0;
+        }
+        return stones[n-1];
+    }
+}
