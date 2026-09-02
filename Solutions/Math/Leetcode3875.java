ff958cfe3e5aaf936ad6a8640a29bfe10d215539
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-09-03 00:16:56 +0530

    Add uniformArray method in Leetcode3875
    Implemented a method to check if an array is uniform.

---
+++ Solutions/Math/Leetcode3875.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+// simple fact if any odd in array minus of that odd with any even becomes odd and if no odd in array then its already all even so its always true
