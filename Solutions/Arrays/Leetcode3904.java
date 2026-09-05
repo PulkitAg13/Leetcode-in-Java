09e12854a783135ca64587baa948422f6270444c
Pulkit Agrawal <162620204+PulkitAg13@users.noreply.github.com>
2026-09-05 12:47:07 +0530

    Implement firstStableIndex method in Leetcode3904

---
+++ Solutions/Arrays/Leetcode3904.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        int max_value = Integer.MIN_VALUE;
+        int min_value = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max_value){
+                max_value = nums[i];
+            }
+            if(nums[nums.length-i-1] < min_value){
+                min_value = nums[nums.length-i-1];
+            }
+            max[i] = max_value;
+            min[nums.length-i-1] = min_value;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(max[i] - min[i] <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
